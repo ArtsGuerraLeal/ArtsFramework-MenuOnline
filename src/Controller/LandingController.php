83c8cac2a77eb7ff30fa7f39f7e8144ae510942e
artguerra <guerraarturo11@gmail.com>
2020-11-28 02:14:49 -0600

    [Menu] Added tags to landing page, moved menu link to root

--- src/Controller/LandingController.php
+++ src/Controller/LandingController.php
@@ -2,7 +2,9 @@
 
 namespace App\Controller;
 
+use App\Repository\MenuRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 /**
@@ -17,6 +19,21 @@ class LandingController extends AbstractController
     {
         return $this->render('landing/index.html.twig', [
             'controller_name' => 'LandingController',
+        ]);
+    }
+
+    /**
+     * @Route("/{name}", name="menu_vie_name", methods={"GET"})
+     * @param MenuRepository $menuRepository
+     * @param $name
+     * @return Response
+     */
+    public function ViewName(MenuRepository $menuRepository,$name): Response
+    {
+        $menu = $menuRepository->findOneBy(['name'=>$name]);
+
+        return $this->render('menu/view.html.twig', [
+            'menu_data' => $menu,
         ]);
     }
 }
