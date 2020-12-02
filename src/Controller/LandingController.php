cce98b409c5d9d1e302625592b16ba35f61e2d6e
Arturo Guerra <guerraarturo11@gmail.com>
2020-12-01 22:10:07 -0600

    [Landing] Added meta tags

--- src/Controller/LandingController.php
+++ src/Controller/LandingController.php
@@ -31,6 +31,8 @@ class LandingController extends AbstractController
      */
     public function index()
     {
+
+
         return $this->render('landing/index.html.twig', [
             'controller_name' => 'LandingController',
         ]);
